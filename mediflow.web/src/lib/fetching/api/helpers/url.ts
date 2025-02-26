85184bcaf6c304382dd29ab5750ab7eb162dd7a1
Valters <valters51423@gmail.com>
2025-02-26 14:06:57 +0200

    communication to api only via same host

--- mediflow.web/src/lib/fetching/api/helpers/url.ts
+++ mediflow.web/src/lib/fetching/api/helpers/url.ts
@@ -1,5 +1,4 @@
-export const baseUrl = process.env.NEXT_PUBLIC_API_URL;
-console.log(process.env.TEST_VAR);
+export const baseUrl = '';
 
 export const baseUrlWithPath = (path: string) => {
 	if (!path.startsWith('/')) {
